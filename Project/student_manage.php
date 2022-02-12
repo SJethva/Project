5cea0810bdd75913acd94a15fadf3dc7d1b5319d
SAPNA JETHVA <77524993+SJethva@users.noreply.github.com>
2022-02-12 22:10:46 +0530

    Update student_manage.php

--- Project/student_manage.php
+++ Project/student_manage.php
@@ -12,7 +12,7 @@
 	$edit_id = isset($_GET['edit_id'])?$_GET['edit_id']:"";
 
 	// get data from the form
-	$name=$_POST['movie_name'];
+	$movie_name=$_POST['movie_name'];
 	$actor=$_POST['actor'];
 	$actress=$_POST['actress'];
 	$director=$_POST['director'];
@@ -33,7 +33,7 @@
       
 		if($edit_id)
 		{	
-			$qry="UPDATE movie SET name='$name',actor='$actor',actress='$actress',director='$director',year='$year' WHERE id=$edit_id";
+			$qry="UPDATE movie SET movie_name='$movie_name',actor='$actor',actress='$actress',director='$director',year='$year' WHERE id=$edit_id";
 			$connection->query($qry);
 			header("location:index.php");
 			exit;
@@ -42,7 +42,7 @@
 
 	/**************************INSERT************************************/
  	
- 		$qry="INSERT INTO movie values('NULL','$name','$actor','$actress','$director','$year')";
+ 		$qry="INSERT INTO movie values('NULL','$movie_name','$actor','$actress','$director','$year')";
 
 		$connection->query($qry);
 		header("location:index.php");
